5a1283292a7b568880b39eaf71d3fc8873efcbd1
Usman Saleem <usman@usmans.info>
2019-11-14 22:52:28 +1000

    ajax call for page

--- js/main.js
+++ js/main.js
@@ -6,18 +6,38 @@ $(document).ready(function() {
     }).then(function(data) {
        $('.blogMeta-pageCount').append(data.pageCount);
        $('.blogMeta-blogCount').append(data.blogCount);
+       pageCallback(1);
 
        $('#pagination').materializePagination({
         align: 'center',
         lastPage:  data.pageCount,
         firstPage:  1,
         urlParameter: 'page',
-        useUrlParameter: true,
+        useUrlParameter: false,
         onClickCallback: pageCallback
     }); 
     });
 });
 
 function pageCallback(requestedPage) {
-    console.log('Requested page is '+ requestedPage);
+    $.ajax({
+        url: "/rest/blog/blogItems/" + requestedPage
+    }).then(function(data) {
+        var cardHtml = "";
+        $.each(data, function(index, element) {
+            var blogItem = '<div class="card">' + 
+            '   <div class="card-content">' +
+            '     <span class="card-title">' + element.title + '</span>' +
+            '     <p>' + element.description + '</p>' +
+            '   </div>' +
+            '   <div class="card-action">' +
+            '     <a class="waves-effect waves-light btn blue-grey darken-1" href="/view/blog/' + element.urlFriendlyId + '">Read More</a>' +
+            '   </div>' +
+            ' </div>';
+            console.log(blogItem);
+            cardHtml += blogItem;
+        });
+
+        $('#cards-div').html(cardHtml);
+    });
 };
